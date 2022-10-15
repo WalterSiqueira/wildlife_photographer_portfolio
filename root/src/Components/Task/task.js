d05b69b60e8c060526932fd2bfb0f1e445365ea1
walter siqueira <walterbarbozasiqueira@gmail.com>
2022-10-15 14:29:07 -0300

    v0.0.4 half way to the delete function

--- root/src/Components/Task/task.js
+++ root/src/Components/Task/task.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import check from '../../Assets/Images/square-check-solid.svg';
 import trash from '../../Assets/Images/trash-solid.svg';
 
-function Task() {
+function Task( {inputText, text, id} ) {
+    const targetChild = useRef();
+    const siblingFinder = (id) => {
+      var siblings = targetChild.current.parentNode;
+      var granpa = [siblings.parentNode.children];
+      var granpsChildren = [...granpa];
+      var childId = null;
+      console.log(siblings.children);
+      console.log(granpa);
+      console.log(granpsChildren.length);
+      for (var i = granpsChildren.length; i--;) {
+        if (granpsChildren[i].props.id) {
+          childId = granpsChildren[i];
+          console.log(childId);
+          break;
+        }
+      }
+      console.log(childId);
+}
     return (
-        <div className='taskListBlock'>
-            <p className='titleTaskList'></p>
-            <button className='checkBTN'>  
+        <div id={id} className='taskListBlock'>
+            <li className='titleTaskList'>{text}</li>
+            <button onClick={siblingFinder} id={text} ref={targetChild} className='checkBTN'>  
                 <img className='checkIcon' src={check}/>
             </button>
-            <button className='trashBTN'>
+            <button className='trashBTN' id={text}>
                 <img className='trashIcon' src={trash}/>
             </button>
         </div>
