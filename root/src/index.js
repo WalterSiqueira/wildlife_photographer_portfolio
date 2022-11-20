7f71c5c03cc5c9f3b00da72cfe1617c826b4a745
WalterSiqueira <walter.siqueira2@outlook.com>
2022-11-20 14:52:40 -0300

    Delete root directory

--- root/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Test from './App';
-import './index.css';
-
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container)
-root.render(<Test/>);
